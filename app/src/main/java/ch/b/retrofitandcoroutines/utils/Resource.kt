c1df89291a8cc61052bb60c7036673f90b1e234f
Dizze222 <lucia31@mail.ru>
2021-09-27 17:31:31 +0300

    Add items in the novetly

--- app/src/main/java/ch/b/retrofitandcoroutines/utils/Resource.kt
+++ app/src/main/java/ch/b/retrofitandcoroutines/utils/Resource.kt
@@ -5,8 +5,9 @@ class Resource<out T>(val status: Status,val data:T,val message: String?) {
     companion object {
         fun<T> success(data: T): Resource<T> = Resource(
             status = Status.SUCCESS,
-            data = data,message = null)
-//https://blog.mindorks.com/using-retrofit-with-kotlin-coroutines-in-android
+            data = data,
+            message = null)
+
         fun<T> error(data: T?,message: String) : Resource<T?> = Resource(
             status = Status.ERROR,
             data = data,
@@ -18,6 +19,8 @@ class Resource<out T>(val status: Status,val data:T,val message: String?) {
             message = null)
 
     }
+
+
 }
 
 
