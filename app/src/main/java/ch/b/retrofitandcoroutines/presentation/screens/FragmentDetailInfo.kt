0bfc7b093d35de76bc310e529a89ed4911c96d0c
Dizze222 <lucia31@mail.ru>
2021-12-09 21:55:34 +0300

    update all dependences

---
+++ app/src/main/java/ch/b/retrofitandcoroutines/presentation/screens/FragmentDetailInfo.kt
@@ -0,0 +1,24 @@
+package ch.b.retrofitandcoroutines.presentation.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ch.b.retrofitandcoroutines.R
+
+class FragmentDetailInfo : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_detail_info, container, false)
+    }
+}
