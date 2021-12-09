d593e1b53f675a7d6416cd047bdec44242ebab60
Dizze222 <lucia31@mail.ru>
2021-12-09 21:31:30 +0300

    deleted all package

--- app/src/main/java/ch/b/retrofitandcoroutines/data/api/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package ch.b.retrofitandcoroutines.data.api
-
-import ch.b.retrofitandcoroutines.data.model.UserDTO
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("list")
-   suspend fun getUsers() : List<UserDTO>
-}
--- app/src/main/java/ch/b/retrofitandcoroutines/ui/main/adapter/AdapterOnClick.kt
+++
@@ -1,8 +0,0 @@
-package ch.b.retrofitandcoroutines.ui.main.adapter
-
-import ch.b.retrofitandcoroutines.data.model.UserDTO
-
-
-interface AdapterOnClick {
-    fun onClick(item: UserDTO)
-}
