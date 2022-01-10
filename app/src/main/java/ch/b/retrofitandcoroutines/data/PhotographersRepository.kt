ca5f7fa0b74258d36a6af2dec010ae9c22ee2d36
Dizze222 <lucia31@mail.ru>
2022-01-10 11:44:19 +0300

    write Junit test for Abstract class

--- app/src/main/java/ch/b/retrofitandcoroutines/data/PhotographersRepository.kt
+++ app/src/main/java/ch/b/retrofitandcoroutines/data/PhotographersRepository.kt
@@ -1,6 +1,5 @@
 package ch.b.retrofitandcoroutines.data
 
-import android.util.Log
 import ch.b.retrofitandcoroutines.data.cache.PhotographersCacheDataSource
 import ch.b.retrofitandcoroutines.data.cache.PhotographersCacheMapper
 import ch.b.retrofitandcoroutines.data.net.PhotographersCloudMapper
@@ -8,12 +7,13 @@ import ch.b.retrofitandcoroutines.data.net.PhotographersDataSource
 
 interface PhotographersRepository {
     suspend fun getPhotographers() : PhotographersData
+    suspend fun getDataFromServerAndSaveIntoDataBase() : PhotographersData
     class Base(
         private val cloudDataSource: PhotographersDataSource,
         private val cacheDataSource: PhotographersCacheDataSource,
         private val photographersCloudMapper: PhotographersCloudMapper,
         private val photographersCacheMapper: PhotographersCacheMapper) : PhotographersRepository{
-        private suspend fun getDataFromServerAndSaveIntoDataBase() : PhotographersData{
+        override suspend fun getDataFromServerAndSaveIntoDataBase(): PhotographersData {
             val photographerCloudList = cloudDataSource.getPhotographers()
             val photographers = photographersCloudMapper.map(photographerCloudList)
             cacheDataSource.savePhotographers(photographers)
@@ -22,21 +22,18 @@ interface PhotographersRepository {
         override suspend fun getPhotographers() : PhotographersData{
             try {
                 val photographerCacheList = cacheDataSource.getPhotographers()
-                if (cloudDataSource.getPhotographers().size > 30){
+                if (cloudDataSource.getPhotographers().size > 30)
                     getDataFromServerAndSaveIntoDataBase()
-                }
-                return if (photographerCacheList.isEmpty()){
+                return if (photographerCacheList.isEmpty())
                     getDataFromServerAndSaveIntoDataBase()
-                }else{
+                else
                     PhotographersData.Success(photographersCacheMapper.map(photographerCacheList))
-                }
             }catch (e: Exception){
                 val photographerCacheList = cacheDataSource.getPhotographers()
-                if (photographerCacheList.isNotEmpty()){
+                if (photographerCacheList.isNotEmpty())
                     return PhotographersData.Success(photographersCacheMapper.map(photographerCacheList))
-                }else{
+                else
                     return PhotographersData.Fail(e)
-                }
             }
         }
     }
