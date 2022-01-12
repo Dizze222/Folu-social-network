e80d2980b7a6948770660932995f1bf8da86e445
Dizze222 <lucia31@mail.ru>
2022-01-12 15:55:44 +0300

    created BaseFragment.kt

---
+++ app/src/main/java/ch/b/retrofitandcoroutines/presentation/BaseFragment.kt
@@ -0,0 +1,7 @@
+package ch.b.retrofitandcoroutines.presentation
+
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment(@LayoutRes layoutResId: Int) : Fragment(layoutResId) {
+}
