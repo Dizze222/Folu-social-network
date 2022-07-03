f6465e2c4306d4f27ed07bc128f1c53dc023e138
Dizze222 <lucia31@mail.ru>
2022-07-03 14:56:32 +0300

    delete not using methods in FavouritesFragment.kt

--- app/src/main/java/ch/b/retrofitandcoroutines/presentation/liked_and_favourites/favourites/FavouritesFragment.kt
+++ app/src/main/java/ch/b/retrofitandcoroutines/presentation/liked_and_favourites/favourites/FavouritesFragment.kt
@@ -1,17 +1,12 @@
 package ch.b.retrofitandcoroutines.presentation.liked_and_favourites.favourites
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import ch.b.retrofitandcoroutines.R
 import ch.b.retrofitandcoroutines.core.PhotoApp
 import ch.b.retrofitandcoroutines.databinding.FragmentFavouritesBinding
-import ch.b.retrofitandcoroutines.presentation.all_posts.stories.StoriesContainerAdapter
 import ch.b.retrofitandcoroutines.presentation.core.BaseFragment
 import javax.inject.Inject
 
